c85363aa1df4783dd178942861b99a6d3f2fb28a
Le Xuan Thach <21521421@gm.uit.edu.vn>
2023-09-14 00:14:47 +0700

    Fistday create frontend

---
+++ pages/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>NeWs Bảng tin mới</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="header">
+        </div>
+        <div class="menu">
+            <ul class="list_menu">
+                <li><a href="index.php">Trang Chủ</a></li>
+                <li><a href="#">Thể loại</a></li>
+                <li><a href="#">Thời sự</a></li>
+                <li><a href="#">Video</a></li>
+                <li><a href="#">Thế giới</a></li>
+            </ul>
+        </div>
+        <div id="main">
+            <div class="sidebar">
+                <ul class="list_sidebar">
+                    <li><a href="index.php">Trang Chủ</a></li>
+                    <li><a href="#">Thể loại</a></li>
+                    <li><a href="#">Thời sự</a></li>
+                    <li><a href="#">Video</a></li>
+                    <li><a href="#">Thế giới</a></li>
+                </ul>
+            </div>
+            <div class="maincontent">
+                Main content
+            </div>
+
+        </div>
+        <div class="clear"></div>
+        <div class="footer">
+            <p>footer</p>
+        </div>
+    </div>
+
+</body>
+
+</html>
